b745b7c6bd447190698221483abd552fe66c1fbe
deepsalunkhee <deepsalunkhee@gmail.com>
2024-12-04 17:44:30 +0530

    hello world from spring boot

---
+++ helloWorld/src/main/java/com/helloWorld/helloWorld/Hello.java
@@ -0,0 +1,14 @@
+package com.helloWorld.helloWorld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "The Hello world from spring bott";
+    }
+}
